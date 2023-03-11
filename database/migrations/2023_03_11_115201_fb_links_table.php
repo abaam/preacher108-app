c7112d18e2752bfca9e92781d99f5e3b24d121ea
abaam <abaamgermones0727@gmail.com>
2023-03-11 19:52:39 +0800

    fb_links

---
+++ database/migrations/2023_03_11_115201_fb_links_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddFacebookPhone extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->string('fb_link')->nullable();
+            $table->string('phone')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        $table->dropColumn('fb_link');
+        $table->dropColumn('phone');
+    }
+}
