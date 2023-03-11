dec9db64fbf79cb9c0a2891b458c630c1fb072f3
abaam <abaamgermones0727@gmail.com>
2023-03-11 19:54:57 +0800

    studnets table update

--- database/migrations/2023_03_11_115201_fb_links_table.php
+++ database/migrations/2023_03_11_115201_fb_links_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFacebookPhone extends Migration
+class UpdateFbLinks extends Migration
 {
     /**
      * Run the migrations.
